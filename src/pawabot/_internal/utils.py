62e7f414cb18e9d775c06416956fc84e6a4bd5f9
Timothée Mazzucotelli <dev@pawamoy.fr>
2026-08-10 12:37:03 +0200

    refactor: Use platformdirs instead of xdg

--- src/pawabot/_internal/utils.py
+++ src/pawabot/_internal/utils.py
@@ -1,24 +1,29 @@
-from xdg import XDG_CACHE_HOME, XDG_CONFIG_HOME, XDG_DATA_HOME, XDG_RUNTIME_DIR
+from pathlib import Path
+
+from platformdirs import PlatformDirs
+
+_dirs = PlatformDirs("pawabot", "pawamoy")
 
 
-def get_dir(xdg_dir):
-    path = xdg_dir / "pawabot"
-    if not path.exists():
-        path.mkdir(parents=True)
+def get_cache_dir() -> Path:
+    path = _dirs.user_cache_path
+    path.mkdir(parents=True, exist_ok=True)
     return path
 
 
-def get_cache_dir():
-    return get_dir(XDG_CACHE_HOME)
+def get_config_dir() -> Path:
+    path = _dirs.user_config_path
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
-def get_config_dir():
-    return get_dir(XDG_CONFIG_HOME)
+def get_data_dir() -> Path:
+    path = _dirs.user_data_path
+    path.mkdir(parents=True, exist_ok=True)
+    return path
 
 
-def get_data_dir():
-    return get_dir(XDG_DATA_HOME)
-
-
-def get_runtime_dir():
-    return get_dir(XDG_RUNTIME_DIR)
+def get_runtime_dir() -> Path:
+    path = _dirs.user_runtime_path
+    path.mkdir(parents=True, exist_ok=True)
+    return path
